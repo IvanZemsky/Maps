197c2a0e7b6b6cf80d2e97f49da9c4153522f111
IvanZemsky <i.ivanzemsky2005@gmail.com>
2025-06-22 01:17:40 +0300

    chore: rename repo and paths

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PMaps/',
+  base: '/Maps/',
   plugins: [
     vue(),
     vueDevTools(),
--- vue.config.ts
+++ vue.config.ts
@@ -1,3 +1,3 @@
 export default {
-  publicPath: '/PMaps/',
+  publicPath: '/Maps/',
 }
